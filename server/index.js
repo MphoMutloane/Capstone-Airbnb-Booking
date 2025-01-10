98fff3b3fdc8c3d4a299b86e1b5b584106769cef
Mpho Mutloane <mphomutloane.mm@gmail.com>
2025-01-10 08:38:27 +0200

    moved dotenv to top of index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 
